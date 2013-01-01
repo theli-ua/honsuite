8737fee88e9a836201de11ed03daceb5c5cb5c35
theli <theli.ua@gmail.com>
2013-01-01 15:05:11 +0200

    will play with this template now

--- src/com/theli/honsuite/MasterServerRequest.java
+++ src/com/theli/honsuite/MasterServerRequest.java
@@ -2,11 +2,11 @@ package com.theli.honsuite;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
 import org.lorecraft.phparser.SerializedPhpParser;
 
 import android.os.AsyncTask;
